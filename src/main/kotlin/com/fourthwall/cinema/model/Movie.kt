4e2d76c0ca14bf3abc47b4f7b28f9c01dcf5a253
Hubert Pardel <hubert.pardel@gmail.com>
2024-11-28 17:09:10 +0100

    all entities as data classes

--- src/main/kotlin/com/fourthwall/cinema/model/Movie.kt
+++ src/main/kotlin/com/fourthwall/cinema/model/Movie.kt
@@ -6,28 +6,10 @@ import org.springframework.data.repository.PagingAndSortingRepository
 
 @Table(name = "movies")
 @Entity
-class Movie(
+data class Movie(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Int? = null,
-    @Column(nullable = false) val title: String = "",
-    @Column(name = "imdb_id", nullable = false) val imdbId: String = ""
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Movie
-
-        if (title != other.title) return false
-        if (imdbId != other.imdbId) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = title.hashCode()
-        result = 31 * result + imdbId.hashCode()
-        return result
-    }
-}
+    @Column(nullable = false) val title: String,
+    @Column(name = "imdb_id", nullable = false) val imdbId: String
+)
 
 interface MovieRepository : JpaRepository<Movie, Int>
--- src/main/kotlin/com/fourthwall/cinema/model/Showtime.kt
+++ src/main/kotlin/com/fourthwall/cinema/model/Showtime.kt
@@ -9,40 +9,18 @@ import java.time.LocalTime
 
 @Table(name = "showtimes")
 @Entity
-class Showtime(
+data class Showtime(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Int? = null,
-    @ManyToOne val movie: Movie? = null,
-    @Column(name = "show_date", nullable = false) val showDate: LocalDate? = null,
-    @Column(name = "show_time", nullable = false) val showTime: LocalTime? = null,
+    @ManyToOne val movie: Movie,
+    @Column(name = "show_date", nullable = false) val showDate: LocalDate,
+    @Column(name = "show_time", nullable = false) val showTime: LocalTime,
     @AttributeOverrides(
         value = arrayOf(
             AttributeOverride(name = "amount", column = Column(name = "price_amount")),
             AttributeOverride(name = "currency", column = Column(name = "price_currency"))
         )
-    ) val price: Money? = null
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Showtime
-
-        if (movie != other.movie) return false
-        if (showDate != other.showDate) return false
-        if (showTime != other.showTime) return false
-        if (price != other.price) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = movie?.hashCode() ?: 0
-        result = 31 * result + (showDate?.hashCode() ?: 0)
-        result = 31 * result + (showTime?.hashCode() ?: 0)
-        result = 31 * result + (price?.hashCode() ?: 0)
-        return result
-    }
-}
+    ) val price: Money
+)
 
 @Embeddable
 data class Money(val amount: BigDecimal? = null, @Enumerated(EnumType.STRING) val currency: Currency? = null)
